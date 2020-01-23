6d18a75e45851a25805491e0428825af75a46a04
Lucas Bastos <lucsbasto@gmail.com>
2020-01-23 15:45:04 -0300

    adicionado configuração eslint e configuração do sequelize

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const routes = require('./routes');
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
