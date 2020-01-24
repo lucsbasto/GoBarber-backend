71cb6b7122392f340e14faa3e585d18ad2933c05
Lucas Bastos <lucsbasto@gmail.com>
2020-01-24 13:30:46 -0300

    criando a pasta app onde vai ficar os controllers e os models

---
+++ src/app/models/User.js
@@ -0,0 +1,20 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  // esse sequelize é a conexão do bd
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
