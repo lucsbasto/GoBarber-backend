bf3d1f6c22878102a16fe95debb697a7a14d02c7
Lucas Bastos <lucsbasto@gmail.com>
2020-01-24 14:27:41 -0300

    adicionando lógica de negocio no controller de user e chamando esse controller na rota

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello bro' });
-});
+routes.post('/users', UserController.store);
 
 module.exports = routes;
