b2d25ac5b9eacdcf5dbec6e4175fa6c708f451cc
Lucas Bastos <lucsbasto@gmail.com>
2020-01-24 14:27:01 -0300

    adicionando hook para fazer o hash da senha antes de salvar no banco

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
@@ -7,6 +8,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -14,6 +16,12 @@ class User extends Model {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
   }
 }
 
