d62249294f1d32d6e1d10d829b9b801b7d97ff9c
Lucas Bastos <lucasbastos@urpay.com.br>
2020-01-25 11:39:23 -0300

    adicionando validação de dados

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,18 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().required(),
+      password: Yup.string().required(),
+    });
     const { email, password } = req.body;
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const user = await User.findOne({ where: { email } });
     if (!user) {
       res.status(401).json({ error: 'User not found' });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,13 +1,72 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const userExists = await User.findOne({ where: { email: req.body.email } });
     if (userExists) {
-      return res.status(400).json({ error: 'User already exists !' });
+      return res.status(401).json({ error: 'User already exists !' });
     }
     const { id, name, email, provider } = await User.create(req.body);
     return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      old_password: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('old_password', (old_password, field) =>
+          old_password ? field.required() : field
+        ),
+      //  oneOf recebe um Yup.ref que é uma função para se referenciar a um outro valor do schema
+      confirm_password: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { email, old_password, password } = req.body;
+    if (password && !old_password) {
+      return res
+        .status(400)
+        .send({ error: 'Validation fails, old password not informed' });
+    }
+    const user = await User.findByPk(req.user_id);
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+      if (userExists) {
+        return res.status(401).json({ error: 'Email already used !' });
+      }
+    }
+    if (old_password && !(await user.checkPassword(old_password))) {
+      return res.status(401).json({ error: 'Password does not match !' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+    return res.status(200).json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
