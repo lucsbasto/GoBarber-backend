669916239304178524511427eac75f89a6c4889e
Lucas Bastos <lucasbastos@urpay.com.br>
2020-01-25 09:28:05 -0300

    adicionando middleware de autenticação nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authHeader from './app/middlewares/auth';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.use(authHeader);
+routes.put('/users', UserController.update);
 
 module.exports = routes;
