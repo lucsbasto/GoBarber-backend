0191d74046848dc256be1dd3c5837f955f2f3823
Lucas Bastos <lucsbasto@gmail.com>
2020-01-27 12:59:16 -0300

    adicionando Appointment na lista de models do index

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,8 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
-const models = [User, File];
+import Appointment from '../app/models/Appointment';
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
