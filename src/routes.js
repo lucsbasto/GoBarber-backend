0bd9e84a62e0360d394885f7012b4306233c6f83
Lucas Bastos <lucsbasto@gmail.com>
2020-01-27 16:05:26 -0300

    criando rota para listagem da agenda do user provider

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+
 import authHeader from './app/middlewares/auth';
 
 const routes = Router();
@@ -19,5 +21,6 @@ routes.post('/files', upload.single('file'), FileController.store)
 routes.get('/providers', ProviderController.index)
 routes.post('/appointments', AppointmentController.store)
 routes.get('/appointments', AppointmentController.index)
+routes.get('/schedule', ScheduleController.index)
 
 module.exports = routes;
