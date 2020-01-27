27cde6d7164e47722aca3c96a83af9ac76019ef9
Lucas Bastos <lucsbasto@gmail.com>
2020-01-27 17:08:35 -0300

    adicionando rota de index de notification

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authHeader from './app/middlewares/auth';
 
@@ -15,12 +16,20 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
 routes.use(authHeader);
+
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), FileController.store)
-routes.get('/providers', ProviderController.index)
-routes.post('/appointments', AppointmentController.store)
-routes.get('/appointments', AppointmentController.index)
-routes.get('/schedule', ScheduleController.index)
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 module.exports = routes;
