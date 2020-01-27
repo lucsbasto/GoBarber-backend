62f894b21e2cc613100454d996e32e755419676e
Lucas Bastos <lucsbasto@gmail.com>
2020-01-27 16:06:06 -0300

    adicionando listagem da agenda do provider e filtrando por data utilizando o Op.between

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,38 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import User from "../models/User";
+import Appointment from '../models/Appointment';
+
+class ScheduleController{
+  async index(req, res){
+    const checkUserProvider = await User.findOne({
+      where: {
+        id: req.user_id,
+        provider: true
+      }
+    });
+    if(!checkUserProvider){
+      return res.json({error: 'User is not a provider'});
+    }
+    const { date } = req.query;
+
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where:{
+        provider_id: req.user_id,
+        canceled_at: null,
+        date: {
+          //[Op.between] coloca o valor desse operador como chave do json
+          [Op.between]:[startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date']
+    });
+
+    return res.json(appointments);
+
+  }
+}
+
+export default new ScheduleController();
