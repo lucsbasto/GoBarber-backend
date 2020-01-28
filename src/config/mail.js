034c0b334e69554ad3d714a58a8e6d5e60268bfd
Lucas Bastos <lucsbasto@gmail.com>
2020-01-28 11:05:41 -0300

    adicionando configs de Mail e enviando com o controller quando o usuário cancela um agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2ec8a3765142cc',
+    pass: '03348e86f759b0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
